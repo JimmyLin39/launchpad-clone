58a9f50892e8e459bd6e757517bd0d6622485775
JimmyLin39 <jimmylin0309@email.com>
2020-03-03 19:58:54 -0800

    initial application components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 export default function Application(props) {
-  return <h1>Launch Pad</h1>
+  return (
+    <React.Fragment>
+      <h1>Launch Pad</h1>
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
+    </React.Fragment>
+  )
 }
